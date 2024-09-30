fc2467c9aa63c1f8bbe84d3728d8b5aa927b6176
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-09-30 15:24:52 -0400

    moved uniswap connector into connector folder

--- contracts/dex-connector/connectors/UniswapV2LikeDex.sol
+++ contracts/dex-connector/connectors/UniswapV2LikeDex.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-import {DexConnector} from "./DexConnector.sol";
+import {DexConnector} from "../DexConnector.sol";
 import "hardhat/console.sol";
 
 contract UniswapV2LikeDex is DexConnector {

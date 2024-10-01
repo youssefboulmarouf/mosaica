5214523e79825dad9dc33c5661f9dd42d60a0a66
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-10-01 17:13:46 -0400

    some renaming

--- contracts/dex-connector/connectors/UniswapV2LikeConnector.sol
+++ contracts/dex-connector/connectors/UniswapV2LikeConnector.sol
@@ -6,7 +6,7 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {DexConnector} from "./DexConnector.sol";
 import "hardhat/console.sol";
 
-contract UniswapV2LikeDex is DexConnector {
+contract UniswapV2LikeConnector is DexConnector {
 
     address public routerAddress;
 
@@ -20,7 +20,7 @@ contract UniswapV2LikeDex is DexConnector {
         address[] memory path = new address[](2);
         path[0] = token1;
         path[1] = token2;
-        
+
         IUniswapV2Router02 uniswapV2Router = IUniswapV2Router02(routerAddress);
         uint256[] memory amountsOut = uniswapV2Router.getAmountsOut(amount, path);
         return amountsOut[1];

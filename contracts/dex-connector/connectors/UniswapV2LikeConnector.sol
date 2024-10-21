009c2a8667f79ef2c6e950d4577f4fe15b827470
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2024-10-21 19:22:37 -0400

    implemented virtual method in UniswapV2LikeConnector

--- contracts/dex-connector/connectors/UniswapV2LikeConnector.sol
+++ contracts/dex-connector/connectors/UniswapV2LikeConnector.sol
@@ -3,41 +3,85 @@ pragma solidity ^0.8.24;
 
 import {IUniswapV2Router02} from "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {DexConnector} from "./DexConnector.sol";
+import {MosaicaLib} from "../../MosaicaUtils.sol";
 import "hardhat/console.sol";
 
 contract UniswapV2LikeConnector is DexConnector {
 
+    address public routerAddress;
     IUniswapV2Router02 public router;
 
     constructor(string memory _dexName, address _routerAddress) Ownable(msg.sender) {
         dexName = _dexName;
         enabled = false;
+        routerAddress = _routerAddress;
         router = IUniswapV2Router02(_routerAddress);
     }
 
-    function getPrice(address token1, address token2, uint256 amount) external view override returns(uint) {
-        address[] memory path = pathFinder(token1, token2, router.WETH());
-        uint256[] memory amountsOut = router.getAmountsOut(amount, path);
-        return amountsOut[amountsOut.length - 1];
-    }
-
-    function swapTokens(address token1, address token2, uint256 amount, uint256 minReturn) external override {
-        address[] memory path = pathFinder(token1, token2, router.WETH());
-        router.swapExactTokensForTokens(amount, minReturn, path, msg.sender, block.timestamp);
-    }
-
-    function pathFinder(address token1, address token2, address weth) internal pure returns(address[] memory path) {
-        bool oneOfTokensIsWeth = token1 == weth || token2 == weth;
-        if (oneOfTokensIsWeth) {
-            path = new address[](2);
-            path[0] = token1;
-            path[1] = token2;
-        } else {
-            path = new address[](3);
-            path[0] = token1;
-            path[1] = weth;
-            path[2] = token2;
+    function getPrice(address srcToken, address destToken, uint256 amount) 
+        public view override 
+        differentTokens(srcToken, destToken)
+        returns(uint) 
+        {
+            uint256[] memory amountsOut = router.getAmountsOut(amount, deducePath(srcToken, destToken));
+            return amountsOut[1];
         }
-    }
+
+    function swapEthForTokens(address destToken, uint256 amount, uint slippage) 
+        internal override
+        returns (uint256)
+        {
+            // swap ETH -> Tokens 
+            uint256[] memory destAmount = router.swapExactETHForTokens{ value: msg.value }(
+                getMinAmountExpected(MosaicaLib.ETH_ADDRESS, destToken, amount, slippage), 
+                deducePath(MosaicaLib.ETH_ADDRESS, destToken), 
+                msg.sender, 
+                block.timestamp + 30
+            );
+            return destAmount[destAmount.length - 1];
+        }
+    
+    function swapTokensForEth(address srcToken, uint256 amount, uint slippage) 
+        internal override
+        returns (uint256)
+        {
+            // swap Tokens -> ETH
+            transferAndApprove(srcToken, address(router), amount);
+            uint256[] memory destAmount = router.swapExactTokensForETH(
+                amount, 
+                getMinAmountExpected(srcToken, MosaicaLib.ETH_ADDRESS, amount, slippage),
+                deducePath(srcToken, MosaicaLib.ETH_ADDRESS), 
+                msg.sender, 
+                block.timestamp + 30
+            );
+            return destAmount[destAmount.length - 1];
+        }
+
+    function swapTokensForTokens(address srcToken, address destToken, uint256 amount, uint256 slippage) 
+        internal override
+        returns (uint256)
+        {
+            // swap Tokens -> Tokens
+            transferAndApprove(srcToken, address(router), amount);
+            uint256[] memory destAmount = router.swapExactTokensForTokens(
+                amount, 
+                getMinAmountExpected(srcToken, destToken, amount, slippage),
+                deducePath(srcToken, destToken),
+                msg.sender, 
+                block.timestamp + 30
+            );
+            return destAmount[destAmount.length - 1];
+        }
+
+    function deducePath(address srcToken, address destToken) 
+        internal pure 
+        returns(address[] memory path) 
+        {
+            // Changing ETH placeholder address to WETH since we can't use ETH_ADDRESS as an ERC20 address
+            path = new address[](2);
+            path[0] = MosaicaLib.isEth(srcToken) ? MosaicaLib.WETH_ADDRESS : srcToken;
+            path[1] = MosaicaLib.isEth(destToken) ? MosaicaLib.WETH_ADDRESS : destToken;
+        }
 }

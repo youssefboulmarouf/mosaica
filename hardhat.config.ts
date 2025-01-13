2b31f279fd2e030387b23ba30b2c936792f13bdb
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-01-13 16:03:53 -0500

    added path to hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,9 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
+  paths: {
+    artifacts: 'client/src/artifacts'
+  },
   solidity: "0.8.27",
 };
 

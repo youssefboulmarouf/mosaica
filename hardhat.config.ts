a0be38a23c708f56ad4a09cc7748750ca0fc4def
YoussefBoulmarouf <youssef.boulmarouf@gmail.com>
2025-02-10 14:45:21 -0500

    added networks details

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,31 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || "";
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
 const config: HardhatUserConfig = {
+  networks: {
+    hardhat: {
+      forking: {
+        url: MAINNET_RPC_URL,
+        blockNumber: 21387857,
+      },
+      gas: 300_000_000,
+      initialBaseFeePerGas: 1_000_000_000,
+      mining: {
+        auto: true,
+      },
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
   paths: {
     artifacts: 'client/src/artifacts'
   },
